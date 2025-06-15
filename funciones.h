f6e024536e2627a9abfe6155dbb28c3c7062b766
Facundo Nahuel Sánchez <facundonahuel2005@gmail.com>
2025-06-15 01:07:05 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -6,5 +6,13 @@
 
  void pedirNumeros();
 
+struct FormaPago {
+    string codigo;  // EF, MP, TR, TC, CT
+    string nombre;
+    int porcentaje;
+};
+
+void CargarFormasPago(FormaPago formasPago[]);
+
 
 #endif // FUNCIONES_H_INCLUDED
