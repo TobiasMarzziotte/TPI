ff5c4949eb14cdb19481c54f916d3928665c7803
TobiasMarzziotte <marzziottetobias@gmail.com>
2025-06-23 22:50:35 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,10 +1,17 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
+#include <string>
+
+using namespace std;
 
 
- int suma(int n1, int n2);
 
- void pedirNumeros();
+// DECLARACION DE STRUCT
+
+struct marca{
+    int codigoMarca;
+    string nombreMarca;
+};
 
 struct FormaPago {
     string codigo;  // EF, MP, TR, TC, CT
@@ -12,7 +19,53 @@ struct FormaPago {
     int porcentaje;
 };
 
+struct Producto{
+    int codProducto;
+    string nombreProducto;
+    float precioVenta;
+    float precioCompra;
+    int stockDisponible;
+    int codMarca;
+};
+
+struct venta{
+    int numeroCompra;
+    int codigoProducto;
+    string formaPago;
+    int cantidadVendida;
+    int codigoCliente;
+    int diaVenta;
+};
+
+struct VentaMarcaForma {
+    int codigoMarca;
+    string codigoFormaPago;
+    int cantidadVendida;
+};
+
+// DECLARACION DE FUNCIONES
+
+void reportePunto2(int contadorVentasPorForma[], int totalVentas, FormaPago formasPago[]);
+
+void reportePunto3(VentaMarcaForma acumulador[], int cantidadAcumulada, FormaPago formasPago[], marca marcas[]);
+
+int menuPrincipal();
+
+int menuReportes(); //declaración para el menú de reportes
+
+void mayusculas(string &str);
+
 void CargarFormasPago(FormaPago formasPago[]);
+
+void cargarMarcas(marca marcas[], int cantidad);
+
+void cargarVentas(int contadorVentasPorForma[], int& totalVentas, FormaPago formasPago[], VentaMarcaForma acumulador[], int &cantAcumuladas, Producto productos[], int ventasPorProducto[]);
+
+bool cargaProductos(Producto productos[]); //contadorVentasPorForma, totalVentas, formasPago, acumulador,cantidadAcumulada, productos ,ventasPorProducto
+
+void reporteSinVentas(Producto productos[], int ventasPorProducto[]);
+
+
 
 
 #endif // FUNCIONES_H_INCLUDED
